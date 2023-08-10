a8d4ddfb22e3458a1f3d5842c163495f49342057
Chris Williams <spider.mane.web@gmail.com>
2023-08-10 19:34:06 -0400

    build config maintenance and added docker

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -4,22 +4,23 @@ use PhpCsFixer\Config;
 use PhpCsFixer\Finder;
 
 $finder = Finder::create()
-    ->in(['app', 'tests'])
-    ->exclude(['bin', 'vendor', 'assets'])
+    ->in(['src', 'tests', 'bin'])
+    ->exclude(['build', 'vendor'])
     ->name('*.php')
+    ->append([basename(__FILE__)])
     ->ignoreDotFiles(true)
     ->ignoreVCS(true);
 
-return (new Config(':theme_slug'))
-    ->setCacheFile(__DIR__ . '/build/php-cs-fixer/.cache')
+return (new Config())
+    ->setCacheFile('build/php-cs-fixer/.cache')
     ->setFinder($finder)
     ->setRules([
         '@PSR12' => true,
         'array_syntax' => ['syntax' => 'short'],
-        'binary_operator_spaces' => true,
         'blank_line_before_statement' => [
             'statements' => ['break', 'continue', 'declare', 'return', 'throw', 'try'],
         ],
+        'cast_spaces' => true,
         'class_attributes_separation' => [
             'elements' => [
                 'method' => 'one',
@@ -32,26 +33,32 @@ return (new Config(':theme_slug'))
             'spacing' => 'one',
         ],
         'fully_qualified_strict_types' => true,
+        'type_declaration_spaces' => true,
         'global_namespace_import' => [
-            'import_classes' => true
+            'import_classes' => true,
         ],
         'lambda_not_used_import' => true,
         'method_argument_space' => [
             'after_heredoc' => true,
         ],
+        'no_empty_phpdoc' => true,
+        'no_extra_blank_lines' => true,
+        'no_unneeded_import_alias' => true,
         'no_unused_imports' => true,
         'not_operator_with_space' => false,
         'not_operator_with_successor_space' => false,
         'object_operator_without_whitespace' => true,
         'ordered_imports' => [
-            'sort_algorithm' => 'alpha'
+            'sort_algorithm' => 'alpha',
+            'imports_order' => ['class', 'function', 'const'],
         ],
         'phpdoc_scalar' => [
             'types' => ['boolean', 'callback', 'double', 'integer', 'real', 'str'],
         ],
         'phpdoc_single_line_var_spacing' => true,
         'phpdoc_var_without_name' => true,
-        'single_space_after_construct' => true,
+        'single_blank_line_at_eof' => true,
+        'single_space_around_construct' => true,
         'ternary_to_null_coalescing' => true,
         'trailing_comma_in_multiline' => true,
         'trim_array_spaces' => true,
