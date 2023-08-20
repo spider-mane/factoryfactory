1635ad4b3348e2ac410204f8fc0308bc6dd5365c
Chris Williams <spider.mane.web@gmail.com>
2023-08-19 21:03:54 -0400

    phpdoc on `FactoryEngineInterface::generate()` for more detailed signiture

--- src/Interfaces/FactoryEngineInterface.php
+++ src/Interfaces/FactoryEngineInterface.php
@@ -4,5 +4,11 @@ namespace WebTheory\Factory\Interfaces;
 
 interface FactoryEngineInterface
 {
+    /**
+     * @param class-string $class
+     * @param array<string, mixed> $args
+     *
+     * @return object
+     */
     public function generate(string $class, array $args = []): object;
 }
